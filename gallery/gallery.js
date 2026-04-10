7ac78e961b3f49ee8a03c482ec6aa0277dd9cfdb
Alessandro <aledemor03@gmail.com>
2026-04-10 14:56:17 +0200

    fixed footer in gallery

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -95,17 +95,3 @@ function buildLayout() {
 
     }
 }
-// ---------------------------------------------------------------------EMAIL
-const emailEl = document.getElementById("email");
-const email = "aledemor03@gmail.com";
-
-emailEl.addEventListener("click", () => {
-  navigator.clipboard.writeText(email);
-
-  const original = emailEl.textContent;
-  emailEl.textContent = "copiato!";
-
-  setTimeout(() => {
-    emailEl.textContent = original;
-  }, 1500);
-});
--- portfolio.js
+++ portfolio.js
@@ -7,20 +7,6 @@ window.addEventListener("load", () => {
   }, 500);
 });
 
-
-const emailEl = document.getElementById("email");
-const email = "aledemor03@gmail.com";
-
-emailEl.addEventListener("click", () => {
-  navigator.clipboard.writeText(email);
-
-  const original = emailEl.textContent;
-  emailEl.textContent = "copiato!";
-
-  setTimeout(() => {
-    emailEl.textContent = original;
-  }, 1500);
-});
 ScrollReveal().reveal('.intro-text', { delay: 200, origin: 'top', viewOffset: {top: 100}});
 ScrollReveal().reveal('.title', { delay: 200, origin: 'top'});
 ScrollReveal().reveal('.carousel', { delay: 200, origin: 'top'});
